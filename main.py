637cc21b307c9c58c86ee6ed044072eb6ec5357c
janri1969 <checkerok@yahoo.com>
2022-08-04 15:26:03 +0700

    coba pertama

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+Membuat program beli susu dan telur
+"""
+jumlah_total_susu = 100
+jumlah_telur = 200
+print('Ibu berkata "pergi ke toko dan belikan susu 1 botol"')
+print('jika di sana ada telur, "tolong belikan 7 butir"')
+print('Adik menjawab, "baik bu"')
+if jumlah_total_susu > 0 :
+    print('Adik mengecek harganya, dan ternyata uangnya cukup')
+    print("Adik membeli susu 1 botol")
+    print("Adik mengecek telur")
+    if jumlah_telur >= 7:
+        print(f"Adik membeli 7 butir telur")
+        else
+            print(f"Adik membeli {jumlah_telur} butir")
+print("Adik Pulang ke rumah dan melaporkan kegiatannya")
