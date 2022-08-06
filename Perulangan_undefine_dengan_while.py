7a12bc95c916b5424e05a3f9d0667d2a5c39697e
janri1969 <checkerok@yahoo.com>
2022-08-06 15:14:57 +0700

    Final revisi tugas program membaca buku sampai mengerti

--- Perulangan_undefine_dengan_while.py
+++ Perulangan_undefine_dengan_while.py
@@ -3,4 +3,14 @@ Perulangan undefined dengan while
 """
 print('Ibu memerintahkan untuk membaca buku sampai mengerti')
 jumlah_buku=10
-total_buku_sudah_dibaca_dimengerti
+jumlah_buku_sudah_dibaca_dimengerti = 0
+banyak_baca = 0
+print("Kemudian adik mulai membaca bukunya satu persatu :")
+while jumlah_buku_sudah_dibaca_dimengerti < jumlah_buku :
+    jumlah_buku_sudah_dibaca_dimengerti = jumlah_buku_sudah_dibaca_dimengerti + 1
+    print(f'\nAdik sudah membaca buku yang ke {jumlah_buku_sudah_dibaca_dimengerti},')
+    if banyak_baca < 1:
+        print(f'Jika adik belum mengerti, adik membaca buku ke {jumlah_buku_sudah_dibaca_dimengerti} kembali sampai mengerti.')
+        banyak_baca = banyak_baca + 1
+    banyak_baca = 0
+print(f'\nAdik sudah membaca dan mengerti sebanyak {jumlah_buku_sudah_dibaca_dimengerti} buku.')
